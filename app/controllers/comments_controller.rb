8d5eff40f0fefe7f501f5dea935c5a1aca8ab58d
KT <truongmkevin@yahoo.com>
2016-10-31 15:25:35 +0000

    whitelisted post_id

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -69,6 +69,6 @@ class CommentsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def comment_params
-      params.require(:comment).permit(:author, :entry)
+      params.require(:comment).permit(:author, :entry, :post_id)
     end
 end
