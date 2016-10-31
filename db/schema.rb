56211916efd5f895c9e0478776d720e63db84ae4
KT <truongmkevin@yahoo.com>
2016-10-31 15:21:38 +0000

    created assocations for comments to posts

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161031145422) do
+ActiveRecord::Schema.define(version: 20161031151608) do
 
   create_table "comments", force: :cascade do |t|
     t.string   "author"
     t.text     "entry"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "post_id"
   end
 
   create_table "posts", force: :cascade do |t|
